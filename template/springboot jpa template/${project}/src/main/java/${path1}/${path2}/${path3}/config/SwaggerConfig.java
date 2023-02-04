f8e86c2111f48317107b3879eb5c9c5ccd31fef5
hejz <75412985@qq.com>
2023-02-04 12:17:05 +0800

    与vue-admin前端对接

--- template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/config/SwaggerConfig.java
+++ template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/config/SwaggerConfig.java
@@ -1,3 +1,7 @@
+package $
+
+{pPackage}.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
