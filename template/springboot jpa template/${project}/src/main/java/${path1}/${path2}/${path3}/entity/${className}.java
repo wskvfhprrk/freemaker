eba2757b17f20a22dc74c0cc4baacf46af3996bb
hejz <75412985@qq.com>
2023-02-14 06:13:10 +0800

    增加表关系功能——未完成

--- template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/entity/${className}.java
+++ template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/entity/${className}.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 /**
  * ${table.tableComment}实体类
  * author: ${author}
+ * 不使用@Data，原因不要toString（）方法，在jpa中表依赖，易出现循环依赖导致栈溢出情况，这是解决之一，另一种方法在表依速中加入注解@JsonIgnoreProperties(value = {"${className?uncap_first}s"})
  * data: ${.now?date}
  */
 @Data
@@ -43,9 +44,15 @@ public class ${className} implements Serializable{
     private ${column.columnJavaType} ${column.javaBeanName};
     <#else>
     /**
-     * 外键表——${column.pkTableName}中的字段${column.pkColumnName}
+     * 外键表——${column.pkTableName}中的字段${column.pkColumnName}，
+     * 1、@ManyToOne也可能是@OneToOne情况——@ManyToOne外键表中没有外键，
+     *      而@OneToOne在外键表中可能有相对对应的外键也可能没有，
+     *      根据业务需要另一方另一方需要带出可设立外键，否则不用设置外键,
+     *      代码自动生成为了区分@OneToOne另一端则设立外键,一方面不需要带出另一方时可以删除
+     * 2、@ManyToMany需要根据中间表判断—
+     * 3、FetchType.LAZY一般设置为赖加载类型，除非需要改为急切类型：EAGER
      */
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "${column.fkColumnName}")
     private ${column.pkJavaBeanName?cap_first} ${column.javaBeanName};
     </#if>
