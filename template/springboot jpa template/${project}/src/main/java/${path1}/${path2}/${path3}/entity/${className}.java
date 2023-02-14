5f365dba8bdd687842063beb0dc5b27831e46b80
hejz <75412985@qq.com>
2023-02-14 08:18:18 +0800

    增加表关系功能——未完成

--- template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/entity/${className}.java
+++ template/springboot jpa template/${project}/src/main/java/${path1}/${path2}/${path3}/entity/${className}.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
  * 1、不使用@Data，原因不要toString（）方法，在jpa中表依赖，易出现循环依赖导致栈溢出情况，这是解决之一，
  * 2、另一种方法在表依速中加入注解@JsonIgnoreProperties(value = {"${className?uncap_first}"})或@JsonIgnoreProperties(value = {"${className?uncap_first}s"})
  * 3、不要使用自查询——解决办法增加一个主键字段不用关键即可——一般情况这个字段可为空字段，查询时可根据id查询即可
+ * 4、双向hashCode和equels方法会导致循环依赖——需要时才在一方使用（手动添加）
  * data: ${.now?date}
  */
 @Data
@@ -54,7 +55,8 @@ public class ${className} implements Serializable{
      *      代码自动生成为了区分@OneToOne另一端则设立外键,一方面不需要带出另一方时可以删除
      * 2、@ManyToMany需要根据中间表判断—
      * 3、FetchType.LAZY一般设置为赖加载类型，除非需要改为急切类型：EAGER
-     * 4、如果要去重复数据需要手动重写hashcode()和equels()方法——否则hashSet()会失效
+     * 4、如果要去重复数据需要手动重写hashcode()和equels()方法——否则hashSet()会失效，
+     *     但是，双向hashCode和equels方法会导致循环依赖——需要时才在一方使用（手动添加）
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "${column.fkColumnName}")
